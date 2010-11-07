260955c3283af9cce9e962380a8f71430500187d
Peter Jupp <pjskyboy@gmail.com>
2010-11-07 15:14:18 +0000

    setup

--- pjgaelyk/src/main/webapp/WEB-INF/groovy/datetime.groovy
+++
@@ -1,7 +0,0 @@
-log.info "Setting attribute datetime"
-
-request.setAttribute 'datetime', new Date().toString()
-
-log.info "Forwarding to the template"
-
-forward '/datetime.gtpl'
--- pjgrails/plugins/app-engine-0.8.10/scripts/_AppEngineCommon.groovy
+++
@@ -1,8 +0,0 @@
-appEngineSDK=System.getenv("APPENGINE_HOME")
-if(!appEngineSDK && buildConfig.google.appengine.sdk) {
-	appEngineSDK = buildConfig.google.appengine.sdk
-}
-if(!appEngineSDK) {
-	println "No Google AppEngine SDK specified. Either set APPENGINE_HOME in your environment or specify google.appengine.sdk in your grails-app/conf/BuildConfig.groovy file"
-	System.exit(1)
-}
